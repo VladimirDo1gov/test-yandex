815dfbe8674d89a3f14f17dc1ea3db8fe4a10999
VladimirDo1gov <157319892+VladimirDo1gov@users.noreply.github.com>
2024-10-24 03:01:16 +0400

    пока все хорошо

--- scripts/TouchHandler.mjs
+++ scripts/TouchHandler.mjs
@@ -1,11 +1,15 @@
 import AnimationService from "./Services/AnimationService.mjs";
+import ProductService from "./Services/ProductService.mjs";
+import StoreService from "./Services/StoreService.mjs";
+
 const banner = document.querySelector(".banner-section");
 
 let isDragging = false;
 let item = null;
 let shiftX = 0;
 let shiftY = 0;
-let dropTargetId = null;
+let dropTarget = null;
+let state = [];
 
 export function onTouchStart(event) {
     event.preventDefault();
@@ -18,39 +22,75 @@ export function onTouchStart(event) {
 
 export function onTouchMove(event) {
     if (isDragging && item) {
-        item.classList.add("selected-touch");
-
-        const { pageX, pageY, clientX, clientY, screenX, screenY } = event.touches[0];
+        const { clientX, clientY } = event.touches[0];
         shiftX = clientX;
         shiftY = clientY;
 
-        // console.log(shiftX, shiftY);
-        console.log(event.touches);
-        console.log(event.clientX);
-
-        item.style.height = item.clientHeight;
-        item.style.width = item.clientWidth;
+        console.log(document.elementFromPoint(shiftX, shiftY).closest(".cart"));
+        item.classList.add("selected-touch");
         item.style.position = "fixed"; // Если удалить, предметы будут возвращаться на места
         item.style.left = shiftX + "px";
         item.style.top = shiftY + "px";
-        console.log(shiftX, shiftY);
-        console.log(item);
     }
 }
 
 export function onTouchEnd(event) {
     if (isDragging) {
-        isDragging = false;
-        item.classList.remove("selected-touch");
-        item.style.left = "";
-        item.style.top = "";
-        item.style.height = "";
-        item.style.width = "";
-        item.style.position = "";
-        item = null;
-        shiftX = 0;
-        shiftY = 0;
+        drop();
+        updateState();
+        reset();
+    }
+}
+
+function drop() {
+    item.hidden = true;
+    dropTarget = document.elementFromPoint(shiftX, shiftY).closest(".cart");
+    item.hidden = false;
+}
+
+function updateState() {
+    if (dropTarget) {
+        StoreService.store(item.id, state);
+    }
+    StoreService.storeCheck(state);
+    if (StoreService.completed) {
+        ProductService.showBannerButton();
+    }
+}
+
+function insertToCart(item) {
+    item.hidden = true;
+    let requiereElement = document.elementFromPoint(shiftX, shiftY).closest(".cart");
+    item.hidden = false;
+    console.log(1);
+    if (requiereElement === productElements.cart) {
+        console.log(2);
+        ProductService.fillRemoveItemPlace(item);
+        ProductService.addProductIntoCart(item);
+        StoreService.store(item.id, initialState);
+        StoreService.storeCheck(initialState);
+        if (StoreService.completed) {
+            ProductService.removeClassesForProductItem();
+            ProductService.addClassesForProductItem();
+            ProductService.showBannerButton();
+        }
     }
 }
 
 function atMove() {}
+
+function reset() {
+    isDragging = false;
+    item.classList.remove("selected-touch");
+    item.style.left = "";
+    item.style.top = "";
+    item.style.height = "";
+    item.style.width = "";
+    item.style.position = "";
+    item = null;
+    shiftX = 0;
+    shiftY = 0;
+}
+
+// item.style.height = item.clientHeight;
+// item.style.width = item.clientWidth;
