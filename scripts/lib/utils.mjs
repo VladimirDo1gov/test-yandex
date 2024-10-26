53d7bb58cf6e1e862c7be48b5d5c43880fd0058e
VladimirDo1gov <157319892+VladimirDo1gov@users.noreply.github.com>
2024-10-27 00:08:04 +0400

    jsdoc

--- scripts/lib/utils.mjs
+++ scripts/lib/utils.mjs
@@ -1,8 +1,19 @@
+/**
+ * Добавляет указанный класс к указанному элементу
+ * @param {string} item - DOM элемент
+ * @param {string} className - название класса
+ */
 export const addClass = (item, className) => {
     if (!item.classList.contains(className)) {
         item.classList.add(className);
     }
 };
+
+/**
+ * Удаляет указанный класс из указанного элемента
+ * @param {string} item - DOM элемент
+ * @param {string} className - название класса
+ */
 export const removeClass = (item, className) => {
     if (item.classList.contains(className)) {
         item.classList.remove(className);
